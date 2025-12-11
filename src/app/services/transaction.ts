3d1e23ec1b1e0846f8bbf2e56b2c3a2c1d15e126
Evandro Sacramento II <evandro-dev@outlook.com>
2025-12-11 02:18:58 -0300

    [FRONTEND] Implementar componente de Filtro (Datepicker Mês/Ano) que recarrega a grid e os cards de totais ao mudar.
    Alterações em dashboard.ts, dashboard.html, transactions.ts para recuperar os dados de totalGeral, totalPendente e totalPago, criação dos arquicos environments para auxiliar nos testes locais, mudança da URl em auth.ts e em transactions.ts
    
    Fixes AB#616

--- src/app/services/transaction.ts
+++ src/app/services/transaction.ts
@@ -1,24 +1,49 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Transaction } from '../model/transaction.model';
+import { environment } from '../../environments/environment';
+
+
+
+
+export interface ResumeTransactionDTO {
+  totalGeral: number;
+  totalPago: number;
+  totalPendente: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TransactionService {
 
-  private apiUrl = 'https://dimonex.onrender.com/api/transactions';
+  private apiUrl = `${environment.apiUrl}/api/transactions`;
 
   constructor(private http: HttpClient) { }
+
+
+
+  getResumeTransactions(mes: number, ano: number): Observable<ResumeTransactionDTO> {
+    const params = new HttpParams()
+      .set('mes', mes.toString())
+      .set('ano', ano.toString());  
+
+    return this.http.get<ResumeTransactionDTO>(`${this.apiUrl}/dashboard/resumo`, { params });
+  }
+
 
   /**
    * Busca TODAS as transações do utilizador autenticado,
    * independentemente do tipo.
    */
-  getAllMyTransactions(): Observable<Transaction[]> {
-    // Este endpoint é mais genérico e será a nossa fonte principal de dados para o dashboard.
-    return this.http.get<Transaction[]>(`${this.apiUrl}`);
+  getAllMyTransactions(mes: number, ano: number): Observable<Transaction[]> {
+    const params = new HttpParams()
+      .set('mes', mes.toString())
+      .set('ano', ano.toString());
+
+    return this.http.get<Transaction[]>(`${this.apiUrl}`, { params });
   }
 
   getConsumptionTransactions(): Observable<Transaction[]> {
